640b299dd6cf159e5978cba35bf4eed7673b4f33
nguyen-son762 <nguyenson7620000@gmail.com>
2024-07-24 23:12:14 +0700

    update ui

--- app/du-an/page.tsx
+++ app/du-an/page.tsx
@@ -5,7 +5,7 @@ import project from "@/data/project.json";
 const page = () => {
   return (
     <div className="container mx-auto px-2 py-[80px] pt-[40px]">
-      <h1 className="text-2xl md:text-4xl font-medium text-center py-10 pt-[100px] uppercase">
+      <h1 className="text-2xl md:text-4xl font-medium text-center py-10 pt-[60px] md:pt-[100px] uppercase">
         CÁC dự án của Việt Sing
       </h1>
       <div className="grid grid-cols-1 gap-12 md:grid-cols-3">
